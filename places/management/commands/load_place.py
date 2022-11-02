9cc8c23110df6a68f4d1150136d5eb3dd9b01e92
Ykyki4 <jimmypantalones@gmail.com>
2022-11-03 01:05:08 +0300

    add load_place command

---
+++ places/management/commands/load_place.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+from urllib.parse import unquote, urlparse
+
+import requests
+from django.core.management.base import BaseCommand
+from django.core.files.base import ContentFile
+from places.models import Place, Image
+
+
+class Command(BaseCommand):
+    help = 'Команда чтобы добавить новое место. Просто укажите адрес нужного json'
+
+    def add_arguments(self, parser):
+        parser.add_argument('json_url', type=str, nargs='+', help='Адрес нужного вам json.')
+
+    def handle(self, *args, **options):
+        try:
+            for place_url in options['json_url']:
+                place_response = requests.get(place_url)
+                place_response.raise_for_status()
+                place = place_response.json()
+                image_urls = place['imgs']
+                place_created, is_created = Place.objects.get_or_create(
+                    title=place['title'],
+                    lon=place['coordinates']['lng'],
+                    lat=place['coordinates']['lat'],
+                    defaults={
+                        'description_short': place['description_short'],
+                        'description_long': place['description_long'],
+                    }
+                )
+
+                for order, img_url in enumerate(image_urls):
+                    try:
+                        filename = unquote(Path(urlparse(img_url).path).name)
+                        image_response = requests.get(img_url)
+                        image_response.raise_for_status()
+                        image_content = ContentFile(image_response.content)
+
+                        place_image = Image(order_numb=order, place=place_created)
+                        place_image.image.save(filename, content=image_content)
+                    except requests.exceptions.HTTPError:
+                        self.stderr.write(self.style.ERROR(
+                            f'Картинка по адресу {img_url} не найдена'))
+        except requests.exceptions.HTTPError:
+            self.stderr.write(self.style.ERROR(
+                f'Описание локации по адресу {options["json_url"]} не найден'))
--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -32,21 +32,11 @@ DEBUG = env.bool('DEBUG', False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = env.bool('SECURE_SSL_REDIRECT', default=True)
 
 SESSION_COOKIE_SECURE = env.bool('SESSION_COOKIE_SECURE', default=True)
 
 CSRF_COOKIE_SECURE = env.bool('CSRF_COOKIE_SECURE', default=True)
-
-SECURE_HSTS_SECONDS = env.int('SECURE_HSTS_SECONDS', default=31536000)
-
-SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
-    'SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
-
-SECURE_HSTS_PRELOAD = env.bool('SECURE_HSTS_PRELOAD', default=True)
-
-SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
-    'SECURE_CONTENT_TYPE_NOSNIFF', default=True)
 
 # Application definition
 
