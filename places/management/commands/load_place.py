0993c803195526b821a4d6c54d33fcf0048be7e8
Ykyki4 <jimmypantalones@gmail.com>
2022-11-08 11:15:10 +0300

    Make try except shorter

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -27,29 +27,28 @@ class Command(BaseCommand):
                     f'Картинка по адресу {img_url} не найдена'))
 
     def handle(self, *args, **options):
-        try:
-            for place_url in options['json_url']:
+        for place_url in options['json_url']:
+            try:
                 place_response = requests.get(place_url)
                 place_response.raise_for_status()
                 place = place_response.json()
-                image_urls = place.get('imgs')
-                try:
-                    place_created, created = Place.objects.get_or_create(
-                        title=place['title'],
-                        lon=place['coordinates']['lng'],
-                        lat=place['coordinates']['lat'],
-                        defaults={
-                            'description_short': place.get('description_short'),
-                            'description_long': place.get('description_long'),
-                        }
-                    )
-                except KeyError as exception:
-                    self.stderr.write(self.style.ERROR(
-                        f'Недоступно поле "{exception.args[0]}" '))
-                    continue
-                if image_urls:
-                    self.save_images(place_created, image_urls)
-
-        except requests.exceptions.HTTPError:
-            self.stderr.write(self.style.ERROR(
-                f"Описание локации по адресу {options['json_url']} не найден"))
+            except requests.exceptions.HTTPError:
+                self.stderr.write(self.style.ERROR(
+                f"Описание локации по адресу {options['json_url']} не найдено"))
+            image_urls = place.get('imgs')
+            try:
+                place_created, created = Place.objects.get_or_create(
+                    title=place['title'],
+                    lon=place['coordinates']['lng'],
+                    lat=place['coordinates']['lat'],
+                    defaults={
+                        'description_short': place.get('description_short'),
+                        'description_long': place.get('description_long'),
+                    }
+                )
+            except KeyError as exception:
+                self.stderr.write(self.style.ERROR(
+                    f'Недоступно поле "{exception.args[0]}" '))
+                continue
+            if image_urls:
+                self.save_images(place_created, image_urls)
